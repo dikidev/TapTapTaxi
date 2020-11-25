d2186d8df6b18ef4519c602f6a800ddf235b777b
D <d@diki.dev>
2020-11-25 14:13:33 +0000

    Building Road with Sprites
    Tiles render underneath eachother not stacking ontop.

--- public_html/static/js/game/TTTGame.js
+++ public_html/static/js/game/TTTGame.js
@@ -1,8 +1,35 @@
 var TTTGame = (function(){
 
+    var ANGLE = 26.55;
+    var TILE_WIDTH = 68;
+
     function TTTGame(phaserGame) {
         this.game = phaserGame;
+
+        this.arrTiles = [];
+        this.numberOfIterations = 0;
     }
+
+    TTTGame.prototype.generateRoad = function(){
+          // sprite (x, y and key defined above)
+          var sprite = new Phaser.Sprite(this.game, 0, 0, 'tile_road_1');
+          this.game.world.addChildAt(sprite, 0);
+          sprite.anchor.setTo(0.5, 0.5); //set anchor to middle
+          sprite.x = this.game.world.centerX; 
+          sprite.y = this.game.world.centerY;
+          this.arrTiles.push(sprite);
+  
+    };
+
+    TTTGame.prototype.moveTiles = function(){
+        var i = this.arrTiles.length -1;
+        while (i >= 0) {
+            var sprite = this.arrTiles[i];
+            sprite.x -= Math.cos ( ANGLE * Math.PI / 180 );
+            sprite.y += Math.sin( ANGLE * Math.PI / 180 );
+            i--;
+        }
+    };
 
     TTTGame.prototype.init = function() {
         this.game.stage.backgroundColor = '#9bd3e1';
@@ -17,17 +44,19 @@ var TTTGame = (function(){
     };
 
     TTTGame.prototype.create = function() {
-
-        // sprite (x, y and key defined above)
-        var sprite = this.game.add.sprite(0, 0, 'tile_road_1');
-        sprite.anchor.setTo(0.5, 0.5); //set anchor to middle
-        sprite.x = this.game.world.centerX; 
-        sprite.y = this.game.world.centerY;
-
+        this.generateRoad();
+    
     };
 
     TTTGame.prototype.update = function() {
 
+        this.numberOfIterations++;
+        if (this.numberOfIterations > TILE_WIDTH){
+            this.numberOfIterations = 0;
+            this.generateRoad();
+        }
+
+        this.moveTiles();
 
     };
 
